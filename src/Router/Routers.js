cf3c63cae2efb554e0eef88da3d5b6ba47b5d146
김민욱 <minuk3508@gmail.com>
2022-09-22 15:21:49 +0900

    start페이지 제거

--- src/Router/Routers.js
+++ src/Router/Routers.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ArithmeticPage from "../Pages/ArithmeticPage";
-import StartPage from "../Pages/StartPage";
 
 function Routers() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/arithmetic-game" element={<StartPage />} />
-        <Route path="/arithmetic" element={<ArithmeticPage />} />
+        <Route path="/arithmetic-game" element={<ArithmeticPage />} />
       </Routes>
     </BrowserRouter>
   );
